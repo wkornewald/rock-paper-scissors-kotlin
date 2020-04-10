d04c79ef5e4cca30b42e5e95b16fd83f1070e8af
Waldemar Kornewald <wkornewald@gmail.com>
2020-04-10 17:19:01 +0200

    Changed unit tests, so they don't just duplicate the original code.
    This is probably more appropriate for the simplified version

--- src/test/kotlin/com/ensody/rockpaperscissors/RockPaperScissors.kt
+++ src/test/kotlin/com/ensody/rockpaperscissors/RockPaperScissors.kt
@@ -9,21 +9,14 @@ import org.junit.jupiter.api.Test
 class RockPaperScissorsTest {
     @Test
     fun rockPaperScissors() {
-        val wins = setOf(
-            Action.ROCK to Action.SCISSORS,
-            Action.PAPER to Action.ROCK,
-            Action.SCISSORS to Action.PAPER
-        )
-
-        for (fighter in Action.values()) {
-            for (enemy in Action.values()) {
-                val result = fighter.fight(enemy)
-                when {
-                    fighter == enemy -> assertThat(result).isEqualTo(FightResult.DRAW)
-                    fighter to enemy in wins -> assertThat(result).isEqualTo(FightResult.WIN)
-                    else -> assertThat(result).isEqualTo(FightResult.LOSE)
-                }
-            }
-        }
+        assertThat(Action.ROCK.fight(Action.ROCK)).isEqualTo(FightResult.DRAW)
+        assertThat(Action.ROCK.fight(Action.PAPER)).isEqualTo(FightResult.LOSE)
+        assertThat(Action.ROCK.fight(Action.SCISSORS)).isEqualTo(FightResult.WIN)
+        assertThat(Action.PAPER.fight(Action.ROCK)).isEqualTo(FightResult.WIN)
+        assertThat(Action.PAPER.fight(Action.PAPER)).isEqualTo(FightResult.DRAW)
+        assertThat(Action.PAPER.fight(Action.SCISSORS)).isEqualTo(FightResult.LOSE)
+        assertThat(Action.SCISSORS.fight(Action.ROCK)).isEqualTo(FightResult.LOSE)
+        assertThat(Action.SCISSORS.fight(Action.PAPER)).isEqualTo(FightResult.WIN)
+        assertThat(Action.SCISSORS.fight(Action.SCISSORS)).isEqualTo(FightResult.DRAW)
     }
 }
